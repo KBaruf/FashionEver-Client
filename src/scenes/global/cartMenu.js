8bc714ce2765719069ec28ee933f3c82cd1c0959
Baruf Kosgei <barufkosgei@gmail.com>
2023-01-20 18:41:12 -0600

    build: create Navbar

---
+++ src/scenes/global/cartMenu.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const cartMenu = () => {
+  return <div>Cart Menu</div>;
+};
+
+export default cartMenu;
---
+++ src/scenes/itemDetails/ItemDetails.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ItemDetails = () => {
+  return <div>Item Detail</div>;
+};
+
+export default ItemDetails;
